f21bd26d1267a3fa9af17b9f8e3c6b4c1d202355
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-03-20 21:28:19 +1100

    Fix language server

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,11 +1,12 @@
-local lsp = require('lsp-zero').preset({
-  name = 'minimal',
-  set_lsp_keymaps = true,
-  manage_nvim_cmp = true,
-  suggest_lsp_servers = false,
-})
-
--- (Optional) Configure lua language server for neovim
-lsp.nvim_workspace()
+local lsp = require('lsp-zero').preset("recommended")
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
