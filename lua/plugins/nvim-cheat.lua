96b751d2c18b94dfe3d3ddc59f5195d743ccfdd7
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-03-28 11:52:53 +1100

    Add nvim cheat sheet

---
+++ lua/plugins/nvim-cheat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'RishabhRD/nvim-cheat.sh',
+    dependencies = {
+      'RishabhRD/popfix',
+    },
+    -- config = function()
+    --   require('cheat-sheet').setup({})
+    -- end,
+  }
+}
