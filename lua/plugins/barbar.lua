bc43af6511fc3bc141cb70084b49c4c8b5f116fb
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-04-05 11:05:10 +1000

    Switch to barbar

---
+++ lua/plugins/barbar.lua
@@ -0,0 +1,31 @@
+local opts = {
+  -- lazy.nvim can automatically call setup for you. just put your options here:
+  -- insert_at_start = true,
+  -- animation = true,
+  -- …etc
+  animation = false,
+  minimum_padding = 1,
+  maximum_padding = 1,
+  insert_at_end = true,
+  highlight_current = false,
+  icons = {
+    diagnostics = {
+      [vim.diagnostic.severity.ERROR] = { enabled = true, icon = require('gehaktmolen.constants').signs.error .. ' ' },
+      [vim.diagnostic.severity.WARN] = { enabled = true, icon = require('gehaktmolen.constants').signs.warn .. ' ' },
+      [vim.diagnostic.severity.INFO] = { enabled = false },
+      [vim.diagnostic.severity.HINT] = { enabled = false },
+    },
+  },
+}
+
+return {
+  {
+    'romgrk/barbar.nvim',
+    event = 'BufReadPost',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('barbar').setup(opts)
+      vim.keymap.set('n', '<C-n>', '<Cmd>BufferPick<CR>', { desc = 'Navigate to buffer' })
+    end,
+  },
+}
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,20 @@ local function setup_theme()
   require('catppuccin').setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     no_italic = true,
+    highlight_overrides = {
+      mocha = function(mocha)
+        return {
+          BufferCurrent = { bg = mocha.mantle, fg = mocha.text },
+          BufferCurrentERROR = { bg = mocha.mantle, fg = mocha.red },
+          BufferCurrentWARN = { bg = mocha.mantle, fg = mocha.peach },
+          BufferCurrentIndex = { bg = mocha.mantle, fg = mocha.blue },
+          BufferCurrentMod = { bg = mocha.mantle, fg = mocha.yellow },
+          BufferCurrentSign = { bg = mocha.mantle, fg = mocha.blue },
+          BufferCurrentTarget = { bg = mocha.mantle, fg = mocha.red },
+          BufferCurrentIcon = { bg = mocha.mantle },
+        }
+      end,
+    },
     integrations = {
       telescope = true,
       treesitter = true,
@@ -10,8 +24,9 @@ local function setup_theme()
       native_lsp = {
         enabled = true,
       },
-      nvimtree = true,
+      -- nvimtree = true,
       neotree = true,
+      barbar = true,
     }
   })
 
