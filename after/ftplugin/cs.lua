0509c078c4fcab885fb438def9aee16c80634d2a
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-05-10 09:33:54 +1000

    Fix read only csharp files cant be opened

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -1,7 +1,11 @@
-vim.cmd([[set ff=dos]])
-
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
   desc = 'Remove spaces at end of line',
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[set ff=dos]],
+  desc = 'Set file format to dos',
 })
