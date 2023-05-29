3a4c39fcbf87181674c1837e1c5132abd1b1bc3b
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-05-29 10:25:46 +1000

    Enable mouse hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -3,10 +3,11 @@ return {
     'm4xshen/hardtime.nvim',
     config = function()
       require('hardtime').setup({
+        disable_mouse = false,
         max_count = 3,
         allow_different_key = true,
         help = false,
-        disabled_filetypes = { "neo-tree", "help" },
+        disabled_filetypes = { "neo-tree", "help", "mason" },
       })
     end,
   }
