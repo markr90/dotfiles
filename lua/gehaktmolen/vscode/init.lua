ad8b4fa7e21f64601e1c1ca6d73235cecee26081
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-06-09 19:24:04 +1000

    VS Code improvements

--- lua/gehaktmolen/vscode/init.lua
+++ lua/gehaktmolen/vscode/init.lua
@@ -8,11 +8,15 @@ end
 
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<SPACE>', '<Nop>', { noremap = true })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, desc = 'Go down half a page' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, desc = 'Go up half a page'})
+
 vim.keymap.set('n', '<leader>ee', notify 'workbench.action.focusSideBar', { silent = true })
 vim.keymap.set('n', '<Leader>gr', notify 'references-view.findReferences', { silent = true }) -- language references
-vim.keymap.set('n', '<Leader>dd', notify 'workbench.actions.view.problems', { silent = true }) -- language diagnostics
+vim.keymap.set('n', '<Leader>dd', notify 'editor.action.showHover', { silent = true }) -- language diagnostics
 vim.keymap.set('n', 'gr', notify 'editor.action.goToReferences', { silent = true })
 vim.keymap.set('n', 'gi', notify 'editor.action.goToImplementation', { silent = true })
+vim.keymap.set('n', 'gd', notify 'editor.action.goToTypeDefinition', { silent = true })
 vim.keymap.set('n', '<Leader>rn', notify 'editor.action.rename', { silent = true })
 vim.keymap.set('n', '<Leader>rf', notify 'editor.action.refactor', { silent = true })
 vim.keymap.set('n', '<Leader>fm', notify 'editor.action.formatDocument', { silent = true })
@@ -20,18 +24,13 @@ vim.keymap.set('n', '<Leader>ca', notify 'editor.action.quickFix', { silent = tr
 vim.keymap.set('n', '[d', notify 'editor.action.marker.prev', { silent = true, noremap = true })
 vim.keymap.set('n', ']d', notify 'editor.action.marker.next', { silent = true, noremap = true })
 
-vim.keymap.set('n', '<Leader>fg', notify 'workbench.action.findInFiles', { silent = true }) -- use ripgrep to search files
 vim.keymap.set('n', '<Leader>ew', notify 'workbench.action.toggleSidebarVisibility', { silent = true })
-vim.keymap.set('n', '<Leader>ff', notify 'workbench.action.quickOpen', { silent = true }) -- find files
 vim.keymap.set('n', '<Leader>tw', notify 'workbench.action.terminal.toggleTerminal', { silent = true }) -- terminal window
 
 vim.keymap.set('v', '<Leader>fm', v_notify 'editor.action.formatSelection', { silent = true })
 vim.keymap.set('v', '<Leader>rf', v_notify 'editor.action.refactor', { silent = true })
 
 vim.keymap.set('n', '<Leader>gb', notify 'gitlens.toggleLineBlame', { silent = true })
-
-vim.keymap.set('n', '<C-j>', '<Cmd>Tabprevious<CR>', { silent = true, noremap = true })
-vim.keymap.set('n', '<C-k>', '<Cmd>Tabnext<CR>', { silent = true, noremap = true })
 
 vim.keymap.set('v', '<leader>p', '"_dP', { noremap = true, desc = 'Paste without clearing register' })
 vim.keymap.set('n', '<leader>wp', 'viw"_dP', { noremap = true, desc = 'Paste and replace word' })
--- lua/gehaktmolen/vscode/plugins/init.lua
+++ lua/gehaktmolen/vscode/plugins/init.lua
@@ -4,8 +4,10 @@ return {
   require('plugins.mini-bracketed'),
   require('plugins.mini-jump'),
   require('plugins.mini-split'),
-  require('plugins.nvim-autopairs'),
-  require('plugins.surround'),
+  require('plugins.mini-surround'),
   require('plugins.mini-indentscope'),
+  require('plugins.nvim-autopairs'),
   require('plugins.indent-blankline'),
+  require('plugins.hardtime'),
+  require('plugins.leap'),
 }
