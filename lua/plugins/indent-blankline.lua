097a998bc5d9d6f0b92e98a3eb8d152a6144239e
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-06-16 11:13:01 +1000

    Try highlight colors

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,11 +3,18 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
       char = "│",
       filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
       show_trailing_blankline_indent = false,
       show_current_context = false,
+      char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+        "IndentBlanklineIndent5",
+        "IndentBlanklineIndent6",
+      },
     },
   },
 }
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -15,9 +15,26 @@ return {
         terminalColors = true,       -- define vim.g.terminal_color_{0,17}
         overrides = function(colors)
           return {
-            ["@variable.builtin"] = { italic = false }
+            ["@variable.builtin"] = { italic = false },
+            -- IndentBlanklineChar = { fg = colors.theme.ui.bg_m2 },
+            IndentBlanklineIndent1 = { fg = colors.theme.ui.bg_m3 },
+            IndentBlanklineIndent2 = { fg = colors.theme.ui.bg_m2 },
+            IndentBlanklineIndent3 = { fg = colors.theme.ui.bg_m1 },
+            IndentBlanklineIndent4 = { fg = colors.theme.ui.bg },
+            IndentBlanklineIndent5 = { fg = colors.theme.ui.bg_p1 },
+            IndentBlanklineIndent6 = { fg = colors.theme.ui.bg_p2 },
+            IndentBlanklineIndent7 = { fg = colors.theme.ui.bg_p3 },
           }
         end,
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              }
+            }
+          }
+        }
       })
       vim.cmd([[colorscheme kanagawa]])
     end,
