17c0f26b167a83cf2d2eda612c68ac6097e93efb
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-06-19 09:37:15 +1000

    Fix vscode imports

--- lua/gehaktmolen/vscode/plugins/init.lua
+++ lua/gehaktmolen/vscode/plugins/init.lua
@@ -5,9 +5,8 @@ return {
   require('plugins.mini-jump'),
   require('plugins.mini-split'),
   require('plugins.mini-surround'),
-  require('plugins.mini-indentscope'),
   require('plugins.nvim-autopairs'),
   require('plugins.indent-blankline'),
   require('plugins.hardtime'),
-  require('plugins.leap'),
+  require('plugins.indent-blankline'),
 }
