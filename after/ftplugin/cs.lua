982601ddd005f89314cbd4383abad2d17cb9dd56
Mark Raaijmakers <mark.raaijmakers@renewtrak.com>
2023-07-10 16:31:30 +1000

    dont auto remove whitespace

--- after/ftplugin/cs.lua
+++ after/ftplugin/cs.lua
@@ -1,11 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
-  command = [[%s/\s\+$//e]],
-  desc = 'Remove spaces at end of line',
-})
-
-vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-  pattern = { "*" },
   command = [[set ff=dos]],
   desc = 'Set file format to dos',
 })
